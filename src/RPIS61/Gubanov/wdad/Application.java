ceff9289ce02544619456119f28f761bfdf0d254
Drew <dhdhdh06@mail.ru>
2018-09-23 17:16:11 +0400

    Added entry point

--- src/RPIS61/Gubanov/wdad/Application.java
+++ src/RPIS61/Gubanov/wdad/Application.java
@@ -1,4 +1,7 @@
 package RPIS61.Gubanov.wdad;
 
 public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Dmitriy Gubanov, and I'm not monkey");
+    }
 }
