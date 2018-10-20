b2c325b42c429f84d0f01f8da2e1273b3b1e446d
Drew <dhdhdh06@mail.ru>
2018-10-20 21:44:37 +0400

    xmlTask fixed

--- src/RPIS61/Gubanov/wdad/learnxml/TestXmlTask.java
+++ src/RPIS61/Gubanov/wdad/learnxml/TestXmlTask.java
@@ -1,6 +1,7 @@
 package RPIS61.Gubanov.wdad.learnxml;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -14,29 +15,27 @@ import java.util.Calendar;
 
 public class TestXmlTask {
     public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException, SAXException {
-//        File file = new File("C:\\Users\\пользователь\\IdeaProjects\\starting-monkey-to-human-path" +
-//                "\\src\\RPIS61\\Gubanov\\wdad\\learnxml\\test2.xml");
-//        XmlTask xmlTask = new XmlTask(file);
-        XmlTask xmlTask = new XmlTask("test1");
-        xmlTask.buildingXmlDocument();
-        xmlTask.addDate();
-        xmlTask.addAnOrder();
-        xmlTask.addAnOfficiant("John", "Stones");
-        xmlTask.addAnItem("Chicken wings", "5", "3");
-        xmlTask.addAnItem("Cola", "2", "2");
-        xmlTask.addTotalCost();
-        xmlTask.addAnOrder();
-        xmlTask.addAnOfficiant("John", "Stones");
-        xmlTask.addAnItem("Chicken wings", "5", "30");
-        xmlTask.addAnItem("Cola", "2", "12");
-        xmlTask.addTotalCost();
-        xmlTask.setTagAtribute("restaurant", "name", "McBurgerC");
+        File file = new File("C:\\Users\\пользователь\\IdeaProjects\\starting-monkey-to-human-path" +
+                "\\src\\RPIS61\\Gubanov\\wdad\\learnxml\\test1.xml");
+        XmlTask xmlTask = new XmlTask(file);
+//        XmlTask xmlTask = new XmlTask("test1");
+//        xmlTask.buildingXmlDocument();
+//        xmlTask.addDate();
+//        Element order = xmlTask.addAnOrder();
+//        xmlTask.addAnOfficiant("John", "Stones");
+//        xmlTask.addAnItem("Chicken wings", "5", "3");
+//        xmlTask.addAnItem("Cola", "2", "2");
+//        xmlTask.addTotalCost();
+//        xmlTask.addAnOrder();
+//        xmlTask.addAnOfficiant("John", "Stones");
+//        xmlTask.addAnItem("Chicken wings", "5", "30");
+//        xmlTask.addAnItem("Cola", "2", "12");
+////        xmlTask.addTotalCost();
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2018, Calendar.OCTOBER, 16);
+        calendar.set(2018, Calendar.OCTOBER, 20);
         System.out.println(xmlTask.earningsTotal("John","Stones", calendar));
-//        xmlTask.changeOfficiantName("John", "Stones", "Kate", "Stones");
-//        xmlTask.removeDay(calendar);
-        xmlTask.transformer();
+        xmlTask.changeOfficiantName("John", "Stones", "Kate", "Stones");
+        xmlTask.removeDay(calendar);
 
 //        File file = new File("C:\\Users\\пользователь\\IdeaProjects\\starting-monkey-to-human-path" +
 //                "\\src\\RPIS61\\Gubanov\\wdad\\learnxml\\test1.xml");
--- src/RPIS61/Gubanov/wdad/learnxml/XmlTask.java
+++ src/RPIS61/Gubanov/wdad/learnxml/XmlTask.java
@@ -24,43 +24,81 @@ public class XmlTask {
         this.document = builder.newDocument();
     }
 
-    public XmlTask(File file) throws ParserConfigurationException, IOException, SAXException {
+    public XmlTask(File file) throws ParserConfigurationException, IOException, SAXException, TransformerException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         this.file = file;
         this.document = builder.parse(file);
         //todo check totalcost
+        totalcostValidation();
     }
+
+    private void totalcostValidation() throws TransformerException, FileNotFoundException {
+        NodeList orders = document.getElementsByTagName("order");
+        Element order, totalcost;
+        for (int i = 0; i < orders.getLength(); i++) {
+            order = (Element) orders.item(i);
+            if(order.getElementsByTagName("totalcost").getLength() == 0){
+                totalcost = document.createElement("totalcost");
+                totalcost.setTextContent(String.valueOf(getTotalCost(order)));
+                order.appendChild(totalcost);
+                transformer();
+            } else {
+                totalcost = (Element) order.getElementsByTagName("totalcost").item(0);
+                if(totalcost.getTextContent() == null){
+                    totalcost.setTextContent(String.valueOf(getTotalCost(order)));
+                    transformer();
+                }
+            }
+        }
+    }
+
+    private int getTotalCost(Element order) {
+        int totalcost = 0, quantity, cost;
+        NodeList items = order.getElementsByTagName("item");
+        Element item;
+        for (int i = 0; i < items.getLength(); i++){
+            item = (Element) items.item(i);
+            quantity = Integer.parseInt(item.getTextContent().trim());
+            cost = Integer.parseInt(item.getAttribute("cost"));
+            totalcost += cost * quantity;
+        }
+        return totalcost;
+    }
+
 //todo rename
-    public int countingTotalCostPerOrder() {
-        int totalCostPerOrder = 0, quantity, cost;
-        Element order = (Element) document.getElementsByTagName("order")
+    private int getTotalCostPerLastOrder() {
+        int totalcost = 0, quantity, cost;
+        Element item, order = (Element) document.getElementsByTagName("order")
                 .item (document.getElementsByTagName("order").getLength() - 1);
         NodeList items = order.getElementsByTagName("item");
         for (int i = 0; i < items.getLength(); i++){
-            quantity = Integer.parseInt(items.item(i).getTextContent().trim());
-            cost = Integer.parseInt(items.item(i).getAttributes().item(0).getNodeValue());
-            totalCostPerOrder += cost * quantity;
+            item = (Element) items.item(i);
+            quantity = Integer.parseInt(item.getTextContent().trim());
+            cost = Integer.parseInt(item.getAttribute("cost"));
+            totalcost += cost * quantity;
         }
-        return totalCostPerOrder;
+        return totalcost;
     }
 
     public void addTotalCost(){
         addTagElement("totalcost", "order");
-        setTagText("totalcost", String.valueOf(countingTotalCostPerOrder()));
+        setTagText("totalcost", String.valueOf(getTotalCostPerLastOrder()));
     }
 
     public org.w3c.dom.Document getDocument() {
         return document;
     }
 
-    public Element findDay(Calendar calendar) {
+    private Element findDay(Calendar day) {
         NodeList days = document.getElementsByTagName("date");
         //todo Use Element.getAttribute()
+        Element calendar;
         for(int i = 0; i < days.getLength(); i++) {
-            if(Integer.parseInt(days.item(i).getAttributes().item(0).getNodeValue()) == calendar.get(Calendar.DATE)
-                    && Integer.parseInt(days.item(i).getAttributes().item(1).getNodeValue()) == calendar.get(Calendar.MONTH) + 1
-                    && Integer.parseInt(days.item(i).getAttributes().item(2).getNodeValue()) == calendar.get(Calendar.YEAR)) {
+            calendar = (Element) days.item(i);
+            if(Integer.parseInt(calendar.getAttribute("day")) == day.get(Calendar.DATE)
+                    && Integer.parseInt(calendar.getAttribute("month")) == day.get(Calendar.MONTH) + 1
+                    && Integer.parseInt(calendar.getAttribute("year")) == day.get(Calendar.YEAR)) {
                 return (Element) days.item(i);
             }
         }
@@ -69,100 +107,100 @@ public class XmlTask {
 
     public int earningsTotal(String officiantFirstName, String officiantSecondName, Calendar calendar) {
         int totalCost = 0;
-        Element day = findDay(calendar);
-        NodeList officiants = day.getElementsByTagName("officiant");
-        for(int i = 0; i < officiants.getLength(); i++){
-            Element officiant = (Element) officiants.item(i);
-            officiant.getAttribute("firstname");
-            Element order = officiant.getParentNode();
-            order.getElementsByTagName("totalcost").item(0);
+        Element officiant, order, day = findDay(calendar);
+        if(day != null) {
+            NodeList officiants = day.getElementsByTagName("officiant");
+            for (int i = 0; i < officiants.getLength(); i++) {
+                officiant = (Element) officiants.item(i);
+                order = (Element) officiant.getParentNode();
 
-            if(officiants.item(i).getAttributes().item(0).getNodeValue().equals(officiantFirstName)
-                    && officiants.item(i).getAttributes().item(1).getNodeValue().equals(officiantSecondName)) {
-//                HEEEEEEEEEEEEEEEEELP
-//                System.out.println(officiants.item(i).getParentNode().getLastChild().getPreviousSibling().getTextContent().trim());
-//                System.out.println(officiants.item(i).getParentNode().getChildNodes().item(7).getTextContent().trim());
-//                System.out.println(officiants.item(i).getParentNode().getLastChild().getTextContent().trim());
-                totalCost += Integer.parseInt(officiants.item(i).getParentNode().getLastChild().getTextContent().trim());
-//                totalCost += Integer.parseInt(officiants.item(i).getParentNode().getLastChild().getPreviousSibling().getTextContent().trim());
+                if (officiant.getAttribute("firstname").equals(officiantFirstName)
+                        && officiant.getAttribute("lastname").equals(officiantSecondName)) {
+                    totalCost += Integer.parseInt(order.getElementsByTagName("totalcost").item(0).getTextContent());
+                }
             }
         }
         return totalCost;
     }
 //todo check null
-    public void removeDay(Calendar calendar) {
-        document.getDocumentElement().removeChild(findDay(calendar));
+    public void removeDay(Calendar calendar) throws TransformerException, FileNotFoundException {
+        Element day = findDay(calendar);
+        if(day != null)
+            document.getDocumentElement().removeChild(day);
+        transformer();
     }
 
-    public void changeOfficiantName(String oldFirstName, String oldSecondName, String newFirstName, String newSecondName) {
+    public void changeOfficiantName(String oldFirstName, String oldSecondName, String newFirstName, String newSecondName) throws TransformerException, FileNotFoundException {
         NodeList officiants = document.getElementsByTagName("officiant");
         //todo Use Element.getAttribute() & setAttribute()
-
+        Element officiant;
         for(int i = 0; i < officiants.getLength(); i++){
             //todo Element officiant = (Element) officiants.item(i);
-            if(officiants.item(i).getAttributes().item(0).getNodeValue().equals(oldFirstName)
-                    && officiants.item(i).getAttributes().item(1).getNodeValue().equals(oldSecondName)) {
-                officiants.item(i).getAttributes().item(0).setNodeValue(newFirstName);
-                officiants.item(i).getAttributes().item(1).setNodeValue(newSecondName);
+            officiant = (Element) officiants.item(i);
+            if(officiant.getAttribute("firstname").equals(oldFirstName)
+                    && officiant.getAttribute("lastname").equals(oldSecondName)) {
+                officiant.setAttribute("firstname", newFirstName);
+                officiant.setAttribute("lastname", newSecondName);
             }
         }
+        transformer();
     }
 
     public void buildingXmlDocument() {
         document.appendChild(document.createElement("restaurant"));
     }
 
-    public void buildingXmlDocument(String rootElement) {
-        document.appendChild(document.createElement(rootElement));
-    }
-
-    public void addTagElement(String elementName, String parentName) {
+    private Element addTagElement(String elementName, String parentName) {
         Element child = document.createElement(elementName);
         document.getElementsByTagName(parentName)
                 .item(document.getElementsByTagName(parentName).getLength() - 1).appendChild(child);
-//        child.appendChild(document.createTextNode(" \n"));
+        return child;
     }
 
-    public void setTagAtribute(String tagName, String attrName, String attrValue) {
+    private void setTagAtribute(String tagName, String attrName, String attrValue) {
         Element element = (Element) document.getElementsByTagName(tagName)
                 .item (document.getElementsByTagName(tagName).getLength() - 1);
         element.setAttribute(attrName, attrValue);
     }
 
-    public void setTagText(String tagName, String text){
+    private void setTagText(String tagName, String text){
         Element element = (Element) document.getElementsByTagName(tagName)
                 .item (document.getElementsByTagName(tagName).getLength() - 1);
-//        element.setTextContent('\n' + text + '\n');
         element.setTextContent(text);
     }
 
-    public void transformer() throws TransformerException, FileNotFoundException {
+    private void transformer() throws TransformerException, FileNotFoundException {
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         transformer.transform(new DOMSource(document), new StreamResult(new FileOutputStream(file)));
     }
 
-    public void addAnOrder() {
-        addTagElement("order", "date");
+    public Element addAnOrder() throws TransformerException, FileNotFoundException {
+        Element order = addTagElement("order", "date");
+        transformer();
+        return order;
     }
 
-    public void addAnItem(String name, String cost , String quantity) {
+    public void addAnItem(String name, String cost , String quantity) throws TransformerException, FileNotFoundException {
         addTagElement("item", "order");
         setTagAtribute("item", "name", name);
         setTagAtribute("item", "cost", cost);
         setTagText("item", quantity);
+        transformer();
     }
 
-    public void addAnOfficiant(String firstname, String lastname) {
+    public void addAnOfficiant(String firstname, String lastname) throws TransformerException, FileNotFoundException {
         addTagElement("officiant", "order");
         setTagAtribute("officiant", "firstname", firstname);
         setTagAtribute("officiant", "lastname", lastname);
+        transformer();
     }
 
-    public void addDate() {
+    public void addDate() throws TransformerException, FileNotFoundException {
         addTagElement("date", "restaurant");
         setTagAtribute("date", "year", String.valueOf(Calendar.getInstance().get(Calendar.YEAR)));
         setTagAtribute("date", "month", String.valueOf(Calendar.getInstance().get(Calendar.MONTH) + 1));
         setTagAtribute("date", "day", String.valueOf(Calendar.getInstance().get(Calendar.DATE)));
+        transformer();
     }
 }
