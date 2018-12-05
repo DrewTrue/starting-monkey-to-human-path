1db27c56c3eb824d07338f964a0be2dd00c751d0
Drew <dhdhdh06@mail.ru>
2018-12-05 23:52:34 +0400

    add constants to PreferencesManager, DataSourceFactory is done

--- src/RPIS61/Gubanov/wdad/Application.java
+++ src/RPIS61/Gubanov/wdad/Application.java
@@ -1,21 +1,7 @@
 package RPIS61.Gubanov.wdad;
 
-import com.mysql.jdbc.Driver;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 public class Application {
     public static void main(String[] args) {
         System.out.println("I'm Dmitriy Gubanov, and I'm not monkey");
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant","root", "");
-            conn.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
     }
 }
--- src/RPIS61/Gubanov/wdad/data/managers/Test.java
+++ src/RPIS61/Gubanov/wdad/data/managers/Test.java
@@ -1,10 +1,12 @@
 package RPIS61.Gubanov.wdad.data.managers;
 
+import RPIS61.Gubanov.wdad.learn.xml.Officiant;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.util.Calendar;
 
 public class Test {
     public static void main(String[] args) throws TransformerException, IOException, SAXException, ParserConfigurationException {
@@ -15,6 +17,6 @@ public class Test {
 //        managers.addBindObject("asasdd", "asda");
 //        managers.removeBindObject("asd");
         JDBCDataManager dataManager = new JDBCDataManager();
-        
+        dataManager.earningsTotal(new Officiant("a", "a"), Calendar.getInstance());
     }
 }
